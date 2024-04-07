ca1e7f996b509238fd0d0ef23ee290059b312b36
Jennifer Nordell <subdev338@gmail.com>
2024-04-07 00:51:55 -0400

    accept a number to set the number of times to repeat the message. Stage 3/3 complete

--- Chalkboard Printer/task/task.js
+++ Chalkboard Printer/task/task.js
@@ -3,7 +3,8 @@ const input = require('sync-input');
 const name = input("Enter name: ")
 const surname = input("Enter surname: ");
 const message = input("Enter message: ");
+const repeatTimes = input("Enter number of repeats");
 
-for(let i = 0; i < 5; i++) {
+for(let i = 0; i < repeatTimes; i++) {
     console.log(`This is ${name} ${surname} and ${message}`);
 }
